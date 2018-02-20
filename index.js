c1c7798ba496f43f15c10dfdc23ae31aac07fa82
Fannar <fannargauti@gmail.com>
2018-02-20 16:19:17 +0000

    test

--- index.js
+++ index.js
@@ -30,8 +30,8 @@ const upload = multer({ storage: storage })
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(bodyParser.raw({ type: 'image/jpeg' }))
-app.use(bodyParser.urlencoded({extended: false}));
+//app.use(bodyParser.raw({ type: 'image/jpeg' }))
+//app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'uploads')));
