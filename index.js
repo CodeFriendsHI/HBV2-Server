160d0071d58d548ff87b6cccc136ba1179a0e82d
Fannar <fannargauti@gmail.com>
2018-03-13 15:24:04 +0000

    added cloudinary

--- index.js
+++ index.js
@@ -1,49 +1,62 @@
-const express = require("express");
-const path = require("path");
-const bodyParser = require("body-parser");
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
 
-const { insertIntoDb, getData, getNewest, cleanOld } = require("./db");
+const { insertIntoDb, getData, getNewest, cleanOld } = require('./db');
 
 const app = express();
-const fs = require("fs");
-const util = require("util");
+const fs = require('fs');
+const util = require('util');
+const cloud = require('cloudinary');
+
+cloud.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_APIKEY,
+  api_secret: process.env.CLOUDINARY_APISECRET,
+});
 
 const dummystreams = [
   {
     id: 0,
-    img: "Skeletor.png"
+    img: 'Skeletor.png',
   },
   {
     id: 1,
-    img: "dabbi.jpg"
+    img: 'dabbi.jpg',
   },
   {
     id: 2,
-    img: "simmi.jpg"
+    img: 'simmi.jpg',
   },
   {
     id: 3,
-    img: "steingrimur.jpg"
+    img: 'steingrimur.jpg',
   },
   {
     id: 4,
-    img: "kata.jpg"
-  }
+    img: 'kata.jpg',
+  },
+  {
+    id: 5,
+    img: 'image.png',
+  },
 ];
 
 const readDirAsync = util.promisify(fs.readdir);
 const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 
-const directory = "./uploads";
-const publicRoute = path.join(__dirname, "/public/");
+const directory = './uploads';
+const publicRoute = path.join(__dirname, '/public/');
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
-app.use(bodyParser.json({ limit: "50mb" }));
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
-app.use(express.static(path.join(__dirname, "uploads")));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'uploads')));
 
 async function read(dir) {
   const images = await readDirAsync(dir);
@@ -54,30 +67,31 @@ async function read(dir) {
   return Promise.all(promises).then(data => data);
 }
 
-app.get("/", async (req, res, next) => {
+app.get('/', async (req, res, next) => {
   // eslint-disable-line
   await read(directory)
-    .then(data => {
+    .then((data) => {
       console.info(data);
-      const encodedData = data.map(i => i.toString("base64"));
+      const encodedData = data.map(i => i.toString('base64'));
       // res.send(encodedData) // sendFILE hér skal senda gögnin
 
-      return res.render("index", { data: encodedData });
+      return res.render('index', { data: encodedData });
     })
-    .catch(err => {
-      // eslint-disable-line
-      return res.send("oh no!");
+    .catch(
+      err =>
+        // eslint-disable-line
+        res.send('oh no!')
       //  errorHandler(err, req, res);
-    });
+    );
 });
 
-app.get("/post", (req, res) => {
+app.get('/post', (req, res) => {
   res.send(`
       <img src="data:image/png;base64,${app.locals.currentImage}" />
     `);
 });
 
-app.get("/:id", async (req, res, next) => {
+app.get('/:id', async (req, res, next) => {
   const id = parseInt(req.params.id, 10);
 
   const found = dummystreams.find(s => s.id === id);
@@ -85,56 +99,52 @@ app.get("/:id", async (req, res, next) => {
   if (found) {
     const options = {
       root: publicRoute,
-      dotfiles: "deny",
+      dotfiles: 'deny',
       headers: {
-        "x-timestamp": Date.now(),
-        "x-sent": true
-      }
+        'x-timestamp': Date.now(),
+        'x-sent': true,
+      },
     };
-    res.sendFile(found.img, options, err => {
+    res.sendFile(found.img, options, (err) => {
       if (err) {
         next(err);
       } else {
-        console.info("Sent image: ", found.img);
+        console.info('Sent image: ', found.img);
       }
     });
   } else {
-    res.send("Not found");
+    res.send('Not found');
   }
 });
 
-app.post("/post", async (req, res, next) => {
+app.post('/post', async (req, res, next) => {
   // eslint-disable-line
-  console.info("posted image");
+  console.info('posted image');
   // app.locals.currentImage = req.body.avatar;
-  const { image = "", roomId = 1 } = req.body;
+  const { image = '', roomId = 1 } = req.body;
   await insertIntoDb({ image, roomId });
   return res.status(201).json(roomId);
 });
 
-app.get("/rooms/:roomId", async (req, res, next) => {
+app.get('/rooms/:roomId', async (req, res, next) => {
   // eslint-disable-line
   const data = await getData();
   const { roomId } = data;
   // console.log('APP.LOCALS.CURRENTIMAGE', app.locals.currentImage)
 
-  res.render("images", { data });
+  res.render('images', { data });
 });
 
-app.get("/streams/:id", async (req, res) => {
+app.get('/streams/:id', async (req, res) => {
   const { id } = req.params;
   const data = await getNewest();
-  const { image } = data[0];
-  //console.log("MYND", image);
-  //res.send(image);
-  res.render("oneimage", { image });
+  const image = data[0].image;
+  const result = await cloud.uploader.upload(`data:image/gif;base64,${image}`);
 
-  // return res.send(`
-  //     <img src=data:image/png;base64,${bufferimg}>
-  //   `);
+  return res.send(result.url);
 });
 
-const hostname = "127.0.0.1";
+const hostname = '127.0.0.1';
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.info(`Server running at http://${hostname}:${port}/`);
