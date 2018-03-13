52d686aa0787459b67b5b0249cbcb6ca75fc23b3
Fannar <fannargauti@gmail.com>
2018-03-13 11:41:02 +0000

    send one photo

--- db.js
+++ db.js
@@ -1,12 +1,13 @@
-const { Client } = require('pg');
+const { Client } = require("pg");
 
-const connectionString = process.env.DATABASE_URL || 'postgres://notandi:@localhost/images';
+const connectionString =
+  process.env.DATABASE_URL || "postgres://notandi:@localhost/images";
 
 async function insertIntoDb(data) {
   const values = Object.values(data);
 
   const client = new Client({ connectionString });
-  const text = 'INSERT INTO images(image, roomId) VALUES($1, $2);';
+  const text = "INSERT INTO images(image, roomId) VALUES($1, $2);";
   await client.connect();
   await client.query(text, values);
   await client.end();
@@ -15,7 +16,7 @@ async function insertIntoDb(data) {
 async function getData() {
   const client = new Client({ connectionString });
   await client.connect();
-  const data = await client.query('SELECT * FROM images;');
+  const data = await client.query("SELECT * FROM images;");
   await client.end();
   return data.rows;
 }
@@ -23,7 +24,9 @@ async function getData() {
 async function getNewest() {
   const client = new Client({ connectionString });
   await client.connect();
-  const data = await client.query('SELECT image FROM images WHERE id = (SELECT max(id) FROM images)');
+  const data = await client.query(
+    "SELECT image FROM images WHERE id = (SELECT max(id) FROM images)"
+  );
   await client.end();
   const { rows } = data;
   console.info(rows);
@@ -32,19 +35,19 @@ async function getNewest() {
 
 async function cleanOld() {
   const client = new Client({ connectionString });
-  const queryString = 'DELETE FROM images WHERE id NOT IN (SELECT id FROM images ORDER BY id DESC LIMIT 10)';
+  const queryString =
+    "DELETE FROM images WHERE id NOT IN (SELECT id FROM images ORDER BY id DESC LIMIT 10)";
   await client.connect();
   const data = await client.query(queryString);
   await client.end();
   const { rows } = data;
-  console.info(rows);
+  //console.info(rows);
   return rows;
 }
-
 
 module.exports = {
   insertIntoDb,
   getData,
   getNewest,
-  cleanOld,
+  cleanOld
 };
--- index.js
+++ index.js
@@ -1,56 +1,49 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
 
-const {
-  insertIntoDb,
-  getData,
-  getNewest,
-  cleanOld,
-} = require('./db');
+const { insertIntoDb, getData, getNewest, cleanOld } = require("./db");
 
 const app = express();
-const fs = require('fs');
-const util = require('util');
+const fs = require("fs");
+const util = require("util");
 
 const dummystreams = [
   {
     id: 0,
-    img: 'Skeletor.png',
+    img: "Skeletor.png"
   },
   {
     id: 1,
-    img: 'dabbi.jpg',
+    img: "dabbi.jpg"
   },
   {
     id: 2,
-    img: 'simmi.jpg',
+    img: "simmi.jpg"
   },
   {
     id: 3,
-    img: 'steingrimur.jpg',
+    img: "steingrimur.jpg"
   },
   {
     id: 4,
-    img: 'kata.jpg',
-  },
+    img: "kata.jpg"
+  }
 ];
 
 const readDirAsync = util.promisify(fs.readdir);
 const readFileAsync = util.promisify(fs.readFile);
 
-const directory = './uploads';
-const publicRoute = path.join(__dirname, '/public/');
+const directory = "./uploads";
+const publicRoute = path.join(__dirname, "/public/");
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'uploads')));
-
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "uploads")));
 
 async function read(dir) {
   const images = await readDirAsync(dir);
@@ -61,29 +54,30 @@ async function read(dir) {
   return Promise.all(promises).then(data => data);
 }
 
+app.get("/", async (req, res, next) => {
+  // eslint-disable-line
+  await read(directory)
+    .then(data => {
+      console.info(data);
+      const encodedData = data.map(i => i.toString("base64"));
+      // res.send(encodedData) // sendFILE hér skal senda gögnin
 
-app.get('/', async (req, res, next) => { // eslint-disable-line
-  await read(directory).then((data) => {
-    console.info(data);
-    const encodedData = data.map(i => i.toString('base64'));
-    // res.send(encodedData) // sendFILE hér skal senda gögnin
-
-    return res.render('index', { data: encodedData });
-  }).catch((err) => { // eslint-disable-line
-    return res.send('oh no!');
-    //  errorHandler(err, req, res);
-  });
+      return res.render("index", { data: encodedData });
+    })
+    .catch(err => {
+      // eslint-disable-line
+      return res.send("oh no!");
+      //  errorHandler(err, req, res);
+    });
 });
 
-
-app.get('/post', (req, res) => {
+app.get("/post", (req, res) => {
   res.send(`
       <img src="data:image/png;base64,${app.locals.currentImage}" />
     `);
 });
 
-
-app.get('/:id', async (req, res, next) => {
+app.get("/:id", async (req, res, next) => {
   const id = parseInt(req.params.id, 10);
 
   const found = dummystreams.find(s => s.id === id);
@@ -91,53 +85,53 @@ app.get('/:id', async (req, res, next) => {
   if (found) {
     const options = {
       root: publicRoute,
-      dotfiles: 'deny',
+      dotfiles: "deny",
       headers: {
-        'x-timestamp': Date.now(),
-        'x-sent': true,
-      },
+        "x-timestamp": Date.now(),
+        "x-sent": true
+      }
     };
-    res.sendFile(found.img, options, (err) => {
+    res.sendFile(found.img, options, err => {
       if (err) {
         next(err);
       } else {
-        console.info('Sent image: ', found.img);
+        console.info("Sent image: ", found.img);
       }
     });
   } else {
-    res.send('Not found');
+    res.send("Not found");
   }
 });
 
-
-app.post('/post', async (req, res, next) => { // eslint-disable-line
-  console.info('posted image');
+app.post("/post", async (req, res, next) => {
+  // eslint-disable-line
+  console.info("posted image");
   // app.locals.currentImage = req.body.avatar;
-  const {
-    image = '',
-    roomId = 1,
-  } = req.body;
+  const { image = "", roomId = 1 } = req.body;
   await insertIntoDb({ image, roomId });
   return res.status(201).json(roomId);
 });
 
-app.get('/rooms/:roomId' , async (req, res, next) => { // eslint-disable-line
+app.get("/rooms/:roomId", async (req, res, next) => {
+  // eslint-disable-line
   const data = await getData();
   const { roomId } = data;
   // console.log('APP.LOCALS.CURRENTIMAGE', app.locals.currentImage)
 
-  res.render('images', { data });
+  res.render("images", { data });
 });
 
-app.get('/streams/:id', async (req, res) => {
+app.get("/streams/:id", async (req, res) => {
   const { id } = req.params;
   const data = await getNewest();
-  const { image } = data;
-
-  return res.send(image);
+  const { image } = data[0];
+  console.log("MYND", image);
+  return res.send(`
+      <img src=data:image/png;base64,${image.toString("base64")}>
+    `);
 });
 
-const hostname = '127.0.0.1';
+const hostname = "127.0.0.1";
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.info(`Server running at http://${hostname}:${port}/`);
